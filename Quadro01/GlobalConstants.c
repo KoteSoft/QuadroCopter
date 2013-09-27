895de11d71d5c8a9af6625d8440ea1d650ae5c73
KoteSoft <na-berlin@bk.ru>
2013-09-27 15:05:27 +0300

    Update GlobalConstants.c

--- Quadro01/GlobalConstants.c
+++ Quadro01/GlobalConstants.c
@@ -7,8 +7,8 @@
 
 #include "GlobalConstants.h"
 
-const double g = 9.80665;
-const double P0 = 101325;
+const double_t g = 9.80665;
+const double_t P0 = 101325;
 const double_t Step = 0.004096;	// 0.016384;
 const uint16_t Alarm_Long = 50;
 const double_t Pa2mmHg = 0.00750061683;
